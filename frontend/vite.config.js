908ba1e46adb264b5f3dcd7eaadfd8879a438111
Marcomano28 <masnopuedo2021@gmail.com>
2024-07-08 09:10:17 +0200

    minus docker

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,7 @@
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), 'VITE_');
-  return {
-    plugins: [react()],
-    define: {
-      'import.meta.env': JSON.stringify(env)
-    }
-  }
-});
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
